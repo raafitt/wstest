885d053afff02999b806274a139e8d063008ef8b
raafitt <baymukhametov99@gmail.com>
2025-08-08 17:54:08 +0500

    Добавлен докерфайл в клиент

--- client/src/service/ws.ts
+++ client/src/service/ws.ts
@@ -6,22 +6,25 @@ export function initWebSocket(onMessage: (msg: MessageEvent) => void) {
 
   socket = new WebSocket("ws://localhost:8080");
 
+  //Калбек При подключении 
   socket.onopen = () => {
     console.log("WebSocket connection opened.");
   };
 
   socket.onmessage = onMessage;
 
+  //Калбек При ошибке 
   socket.onerror = (err) => {
     console.error("WebSocket error:", err);
   };
 
+  //При закрытии соединения
   socket.onclose = () => {
     console.log("WebSocket connection closed.");
     socket = null;
   };
 }
-
+//Функция отправки данных на сервер
 export function sendMessage(msg: any) {
   if (socket?.readyState === WebSocket.OPEN) {
     socket.send(JSON.stringify(msg));
